e312fc4ec6e5a3b8c885e59bf1104c239f8ac5f5
ivaniomartins <ivaniomartins1991@gmail.com>
2023-10-10 22:53:33 -0300

    Criando Repository e Service para Categoria

--- src/main/java/com/martins/cursospring/models/Categoria.java
+++ src/main/java/com/martins/cursospring/models/Categoria.java
@@ -1,4 +1,4 @@
-package com.martins.cursospring.model;
+package com.martins.cursospring.models;
 
 import java.io.Serializable;
 import java.util.Objects;
---
+++ src/main/java/com/martins/cursospring/services/CategoriaService.java
@@ -0,0 +1,27 @@
+package com.martins.cursospring.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.martins.cursospring.models.Categoria;
+import com.martins.cursospring.repositories.CategoriaRepository;
+
+@Service	
+public class CategoriaService {
+	@Autowired
+	private CategoriaRepository repository;
+
+	
+     public Categoria buscar(Integer id) {
+    	Optional<Categoria> obj = repository.findById(id);
+          	 
+    	 return obj.orElse(null);
+    	 
+     }
+	
+	
+}
+
+
