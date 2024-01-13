166e4defd070f0e1dc69d22ce6f5889c46991563
ivaniomartins <ivaniomartins1991@gmail.com>
2024-01-13 16:31:59 -0300

    Criando operação de instanciação e atualização as configurações do projeto

--- src/main/java/com/martins/cursospring/resources/CategoriaResource.java
+++ src/main/java/com/martins/cursospring/resources/CategoriaResource.java
@@ -2,9 +2,9 @@ package com.martins.cursospring.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.martins.cursospring.models.Categoria;
@@ -17,7 +17,7 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaService service;
 	
-	@RequestMapping(value="/{id}",method=RequestMethod.GET)
+	@GetMapping("/{id}")
 	public ResponseEntity<?> find(@PathVariable Integer id) {
     		Categoria obj = service.buscar(id);	
 		return ResponseEntity.ok().body(obj);
