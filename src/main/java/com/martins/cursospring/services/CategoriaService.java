cc6d5589cf3353b1c2587d3de4aeaeb17bf0f543
ivaniomartins <ivaniomartins1991@gmail.com>
2024-02-01 13:11:16 -0300

    Ajuste no classe CategoriaResource para retornar a categoria selecionada e implementação do método método findAll() para a CategoriaResource.

--- src/main/java/com/martins/cursospring/services/CategoriaService.java
+++ src/main/java/com/martins/cursospring/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.martins.cursospring.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +21,12 @@ public class CategoriaService {
     	 return obj.orElse(null);
     	 
      }
+     
+ 	public Iterable<Categoria> buscarTodos(){
+		
+ 		return repository.findAll();
+ 			
+ 		}
 	
 	
 }
